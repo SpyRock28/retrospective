7df7fd430cc1f3790ab0bdd0b673f58dc645e11d
SpyRock28 <37314152+SpyRock28@users.noreply.github.com>
2019-01-30 11:56:00 -0700

    set up dashboard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>Retrospective</h1>
+          <Navbar />
+          <Switch>
+            <Route path='/' component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
-    );
+    )
   }
 }
 
---
+++ src/components/retros/RetroList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const RetroList = () => {
+  return (
+    <div className="retro section">
+      <div className="card z-depth-0 retro-summary">
+        <div className="card content grey-text darken-3">
+          <span className="card-title">Retro Title</span>
+          <p>Topic added by SpyRock</p>
+          <p className="grey-text">Jan 30, 2019</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 retro-summary">
+        <div className="card content grey-text darken-3">
+          <span className="card-title">Retro Title</span>
+          <p>Topic added by SpyRock</p>
+          <p className="grey-text">Jan 30, 2019</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 retro-summary">
+        <div className="card content grey-text darken-3">
+          <span className="card-title">Retro Title</span>
+          <p>Topic added by SpyRock</p>
+          <p className="grey-text">Jan 30, 2019</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RetroList;
