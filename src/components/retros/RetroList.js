a05bb304f64b8b5f6e33cf96773d05171337950f
SpyRock28 <37314152+SpyRock28@users.noreply.github.com>
2019-01-30 12:06:06 -0700

    retroSummary added

--- src/components/retros/RetroList.js
+++ src/components/retros/RetroList.js
@@ -1,31 +1,14 @@
 import React from 'react';
+import RetroSummary from './RetroSummary';
 
 const RetroList = () => {
   return (
     <div className="retro section">
-      <div className="card z-depth-0 retro-summary">
-        <div className="card content grey-text darken-3">
-          <span className="card-title">Retro Title</span>
-          <p>Topic added by SpyRock</p>
-          <p className="grey-text">Jan 30, 2019</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 retro-summary">
-        <div className="card content grey-text darken-3">
-          <span className="card-title">Retro Title</span>
-          <p>Topic added by SpyRock</p>
-          <p className="grey-text">Jan 30, 2019</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 retro-summary">
-        <div className="card content grey-text darken-3">
-          <span className="card-title">Retro Title</span>
-          <p>Topic added by SpyRock</p>
-          <p className="grey-text">Jan 30, 2019</p>
-        </div>
-      </div>
+       
+      <RetroSummary />
+      <RetroSummary />
+      <RetroSummary />
+      <RetroSummary />
     </div>
   )
 }
---
+++ src/components/retros/RetroSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RetroSummary = () => {
+  return (
+    <div className="card z-depth-0 retro-summary">
+      <div className="card content grey-text darken-3">
+        <span className="card-title">Retro Title</span>
+        <p>Topic added by SpyRock</p>
+        <p className="grey-text">Jan 30, 2019</p>
+      </div>
+    </div>
+  )
+}
+
+export default RetroSummary;
