c32d7fd3cbc78e35c53db7a25070879d7a280123
SpyRock28 <37314152+SpyRock28@users.noreply.github.com>
2019-01-30 11:14:58 -0700

    Stubbing out files. starting navbar
    Stubbing out files. Starting Navbar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Retrospective</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <h1>Retrospective</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }
