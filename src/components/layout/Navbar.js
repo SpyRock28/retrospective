ecdba3e18283658b8a98ab39b07cabad36588e64
SpyRock28 <37314152+SpyRock28@users.noreply.github.com>
2019-02-01 08:03:57 -0700

    stuff

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,9 +6,10 @@ import SignedOutLinks from './SignedOutLinks';
  
 const Navbar = () => {
   return (
-    <nav className="nav-wrapper grey darken-3">
+    <nav className="nav-wrapper grey darken-2">
       <div className="container">
         <Link to='/' className="brand-logo left">Retrospective</Link>
+        
         <SignedInLinks />
         <SignedOutLinks />
       </div>
---
+++ src/components/retros/RetroDetails.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class RetroDetails extends Component {
+  
+
+  return() {
+    render(
+
+    )
+  }
+}
+
+export default RetroDetails;
